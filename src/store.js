09c07fe1f0be5c82f6f6466ab08c0950c36d7f2c
Gaelle <gaelle@bimdata.io>
2020-01-10 11:09:32 +0100

    segment store.js & main.js

---
+++ src/store.js
@@ -0,0 +1,58 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    'gettingstarted':{
+      text: "Getting Started"
+    },
+    guidelines: {
+      text: "Guidelines for colors, spacing,  typography",
+      children: [
+        {
+          title: "colors",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vitae numquam laboriosam",
+          btn: "View Colors"
+        },
+        {
+          title: "grid",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vitae numquam laboriosam",
+          btn: "View Grid"
+        },
+        {
+          title: "spacing",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vitae numquam laboriosam",
+          btn: "View spacing"
+        },
+        {
+          title: "typography",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vitae numquam laboriosam",
+          btn: "View typography"
+        }
+      ]
+    },
+    components: {
+      text: "some text",
+      children: [
+        {
+          title: "buttons",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vitae numquam laboriosam",
+          btn: "View Buttons"
+        },
+        {
+          title: "icons",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vitae numquam laboriosam",
+          btn: "View icons"
+        }
+      ]
+    }
+  }
+});
