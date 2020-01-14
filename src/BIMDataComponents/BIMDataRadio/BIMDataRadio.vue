1a63e65c1538e613cf8709e1dcf2f7df4e5a3149
Gaelle <gaelle@bimdata.io>
2020-01-14 11:16:17 +0100

    add BIMDataRadio component

---
+++ src/BIMDataComponents/BIMDataRadio/BIMDataRadio.vue
@@ -0,0 +1,57 @@
+<template>
+  <label class="bimdata-radio">
+    <input
+      type="radio"
+      @input="onInput"
+      :checked="selectedValue === value"
+      :name="name"
+      :id="id"
+      :value="value"
+    />
+    <span class="bimdata-radio__text">{{ text }}</span>
+  </label>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: "selectedValue"
+  },
+  props: {
+    text: {
+      type: String,
+      default: null
+    },
+    name: {
+      type: String,
+      default: "option"
+    },
+    id: {
+      type: String
+    },
+    value: {
+      type: String
+    },
+    selectedValue: {
+      type: String
+    }
+  },
+  methods: {
+    onInput() {
+      this.$emit("input", this.value);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+  // import BIMDATA VARIABLES
+  @import "../../assets/scss/_BIMDataVariables.scss";
+
+  // import BIMDATA MIXINS
+  @import "../../assets/scss/mixins/_pseudo.scss";
+  @import "../../assets/scss/mixins/_font-size.scss";
+
+  // import BIMDATA STYLE COMPONENT
+  @import "./BIMDataRadio.scss";
+</style>
