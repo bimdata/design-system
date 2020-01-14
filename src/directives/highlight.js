8025288bf1f5dcda46304fece70e7f1458b80175
Gaelle <gaelle@bimdata.io>
2020-01-14 10:48:17 +0100

    add highlight directive

---
+++ src/directives/highlight.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import Prism from 'prismjs';
+import Normalizer from 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js'
+
+const normalizer = new Normalizer
+
+export default {
+  bind(el, binding, vnode) {
+    highlight(el, binding, vnode);
+  },
+  componentUpdated(el, binding, vnode) {
+    highlight(el, binding, vnode);
+  }
+}
+
+function highlight (el, binding, vnode) {
+  const choosedLanguage = binding.arg;
+  if(!["xml", "scss", "javascript"].includes(choosedLanguage)){
+    throw new Error("Supported languages are xml, javascript and scss");
+  }
+  const textToHighlight = vnode.children[0].text;
+
+  el.innerHTML = normalizer.normalize(Prism.highlight(textToHighlight, Prism.languages[choosedLanguage], choosedLanguage), {
+    'remove-trailing': true,
+    'remove-indent': true,
+  });
+  Vue.nextTick( () => Prism.highlightAll());
+}
