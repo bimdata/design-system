ced2350ae02f381ea49a628a5b60a3b6ce88a8a0
Gaelle <gaelle@bimdata.io>
2020-01-15 17:57:41 +0100

    custom content page

--- src/design-system/views/Layout/Content.vue
+++ src/design-system/views/Layout/Content.vue
@@ -3,16 +3,16 @@
   <main>
     <div class="article-wrapper">
       <h1>{{ $route.name }}</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit excepturi deleniti nemo debitis temporibus dolores, distinctio nam, sed eligendi adipisci enim earum molestiae vero! Consequuntur possimus libero nam perspiciatis placeat?</p>
+      <p>{{$store.state[$route.name].text}}</p>
 
       <div class="content-box">
-        <div class="bimdata-box" v-for="children in $store.state[$route.name]" :key="children.id">
-          <router-link :to="children.title" append>
+        <div class="bimdata-box" v-for="child in $store.state[$route.name].children" :key="child.id">
+          <router-link :to="child.title" append>
             <div class="bimdata-box__content">
-              <h2>{{ children.title }}</h2>
-              <p>{{ children.text }}</p>
+              <h2>{{ child.title }}</h2>
+              <p>{{ child.text }}</p>
               <BIMDataButton class="bimdata-btn bimdata-btn__ghost">
-                {{ children.btn }}
+                {{ child.btn }}
                 <BIMDataIcon class="icon-close" icon-name="close" width="11" height="11" x="13" y="23">
                   <BIMDataChevronRightIcon />
                 </BIMDataIcon>
@@ -28,8 +28,8 @@
 
 <script>
 import BIMDataButton from "../../../BIMDataComponents/BIMDataButton/BIMDataButton.vue";
-import BIMDataIcon from "../../../BIMDataComponents/BIMDataIcon/BIMDataIcon.vue";
-import BIMDataChevronRightIcon from "../../../BIMDataComponents/BIMDataIcon/BIMDataLibraryIcon/BIMDataChevronRightIcon.vue";
+import BIMDataIcon from "../../../BIMDataComponents/BIMDataIcons/BIMDataIcon.vue";
+import BIMDataChevronRightIcon from "../../../BIMDataComponents/BIMDataIcons/BIMDataLibraryIcons/BIMDataChevronRightIcon.vue";
 
 export default {
   components:{
