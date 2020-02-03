b18ac067d608e9a2f04d3e0dbdcef45fc8f06ff0
Gaelle <gaelle@bimdata.io>
2020-02-03 15:20:45 +0100

    create a MarkupStyle for style convention

---
+++ src/design-system/views/views-item/MarkupStyle/MarkupStyle.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h3>BEM Naming</h3>
+    <p>BEM is a well-known method of naming components — block, element, modifier. If you’re comfortable with BEM, move down to the “Where we diverge from BEM” section. For those unfamiliar or who need a quick refresh, let’s briefly look at how BEM works. As an example, we'll build a button component.</p>
+    <h4>Block</h4>
+
+    <h4>Element</h4>
+
+    <h4>Modifier</h4>
+  </div>
+</template>
