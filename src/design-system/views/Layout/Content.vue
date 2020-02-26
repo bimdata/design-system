aad545864ef8e6c3e0789417c845a479e886516d
Gaelle <gaelle@bimdata.io>
2020-02-26 15:27:18 +0100

    custom content page

--- src/design-system/views/Layout/Content.vue
+++ src/design-system/views/Layout/Content.vue
@@ -1,41 +1,37 @@
 <template>
-<div class="article">
-  <main>
-    <div class="article-wrapper">
-      <h1>{{ $route.name }}</h1>
-      <p>{{$store.state[$route.name].text}}</p>
-
-      <div class="content-box">
-        <div class="bimdata-box" v-for="child in $store.state[$route.name].children" :key="child.id">
-          <router-link :to="child.title" append>
-            <div class="bimdata-box__content">
-              <h2>{{ child.title }}</h2>
-              <p>{{ child.text }}</p>
-              <BIMDataButton class="bimdata-btn bimdata-btn__ghost">
-                {{ child.btn }}
-                <BIMDataIcon class="icon-close" icon-name="close" width="11" height="11" x="13" y="23">
-                  <BIMDataChevronRightIcon />
-                </BIMDataIcon>
-              </BIMDataButton>
-            </div>
-          </router-link>
-        </div>
+<main class="article">
+  <div class="article-wrapper">
+    <h1>{{ $route.name }}</h1>
+    <p>{{$store.state[$route.name].text}}</p>
+    <div class="content-box">
+      <div class="bimdata-box" v-for="child in $store.state[$route.name].children" :key="child.id">
+        <router-link :to="child.path" append>
+          <div class="bimdata-box__content">
+            <img :src="child.img"/>
+            <h5>{{ child.title }}</h5>
+            <p>{{ child.text }}</p>
+            <BIMDataButton class="bimdata-btn bimdata-btn__radius bimdata-btn__fill bimdata-btn__fill--primary">
+              {{ child.btn }}
+            </BIMDataButton>
+          </div>
+        </router-link>
       </div>
     </div>
-  </main>
-</div>
+  </div>
+</main>
 </template>
 
 <script>
 import BIMDataButton from "../../../BIMDataComponents/BIMDataButton/BIMDataButton.vue";
-import BIMDataIcon from "../../../BIMDataComponents/BIMDataIcons/BIMDataIcon.vue";
-import BIMDataChevronRightIcon from "../../../BIMDataComponents/BIMDataIcons/BIMDataLibraryIcons/BIMDataChevronRightIcon.vue";
 
 export default {
   components:{
     BIMDataButton,
-    BIMDataIcon,
-    BIMDataChevronRightIcon
+  },
+  methods: {
+    getRouter(){
+      return this.$route.params;
+    }
   }
 }
 </script>
