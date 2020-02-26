3f7b64448d8e91bc01aa42a72c37ea3991dfa13d
Gaelle <gaelle@bimdata.io>
2020-02-26 15:27:29 +0100

    custom aside page

--- src/design-system/views/Layout/Aside.vue
+++ src/design-system/views/Layout/Aside.vue
@@ -4,7 +4,7 @@
       <h2 class="aside__title" @click="onTitleClick">{{ getPathFirstElement() }}</h2>
       <ul class="aside__text bimdata-list">
         <li v-for="child in $store.state[getPathFirstElement()].children" :key="child.id">
-          <router-link :to="{ name: child.title}">{{ child.title }}</router-link>
+          <router-link :to="child.path">{{ child.title }}</router-link>
         </li>
       </ul>
     </div>
