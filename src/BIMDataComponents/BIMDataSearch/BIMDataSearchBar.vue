28dab62ef6c78a79f84d20022f5937203e913f9f
Gaelle <gaelle@bimdata.io>
2020-03-03 18:29:21 +0100

    custom BIMDataSearchBar

--- src/BIMDataComponents/BIMDataSearch/BIMDataSearchBar.vue
+++ src/BIMDataComponents/BIMDataSearch/BIMDataSearchBar.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="bimdata-search-bar">
+  <div class="bimdata-search-bar" v-clickaway="away" :style="{ width: width, height: height }" :class="{'focus': focused}">
     <span class="bimdata-search-icon">
-      <BIMDataIcon icon-name="search" width="18" height="18" x="24" y="24" class="bimdata-fill-text">
+      <BIMDataIcon icon-name="search" width="13" height="13" x="23" y="23" class="bimdata-fill-primary">
         <BIMDataSearchIcon />
       </BIMDataIcon>
     </span>
     <input
-      v-focus
-      :value="value"
+      v-focus="autofocus"
+      @focus="focused = true"
+      @blur="focused = false"
       @input="$emit('input', $event.target.value)"
-      placeholder="Rechercher"
+      :placeholder="placeholder"
     />
   </div>
 </template>
 <script>
+import clickaway from "../../directives/click-away.js";
 
 /* import BIMData ICONS */
 import BIMDataIcon from "../BIMDataIcons/BIMDataIcon.vue";
@@ -23,18 +25,47 @@ export default {
     BIMDataIcon,
     BIMDataSearchIcon
   },
+  data() {
+    return {
+      inputVisible: false,
+      focused: false,
+    };
+  },
   props: {
-    value: {
+    placeholder: {
       type: String,
-      required: true
+      default: ""
+    },
+    width: {
+      type: [Number, String],
+      default: "150px"
+    },
+    height: {
+      type: [Number, String],
+      default: "32px"
+    },
+    autofocus: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    away() {
+      this.inputVisible = false;
+    },
+    focus() {
+      this.$refs.input && this.$refs.input.focus();
     }
   },
   directives: {
     focus: {
-      inserted: function(el) {
-        el.focus();
+      inserted: function(el, {value}) {
+        if (value) {
+          el.focus();
+        }
       }
-    }
+    },
+    clickaway
   },
   watch: {
     text(value) {
