acca99b82c0cbbee6837a77530ebe7d8e74c70e9
Gaelle <gaelle@bimdata.io>
2020-03-04 13:53:38 +0100

    import all icons

--- src/BIMDataComponents/index.js
+++ src/BIMDataComponents/index.js
@@ -1,12 +1,14 @@
 import BIMDataButton from './BIMDataButton/BIMDataButton.vue';
 import BIMDataCheckbox from './BIMDataCheckbox/BIMDataCheckbox.vue';
-import BIMDataIcons from './BIMDataIcons/BIMDataIcon.vue';
+import BIMDataIcon from './BIMDataIcons/BIMDataIcon.vue';
+import BIMDataIcons from './BIMDataIcons/icons.js';
 import BIMDataRadio from './BIMDataRadio/BIMDataRadio.vue';
 import BIMDataSearch from './BIMDataSearch/BIMDataSearchBar.vue';
 
 const Components = {
   BIMDataButton,
   BIMDataCheckbox,
+  BIMDataIcon,
   BIMDataIcons,
   BIMDataRadio,
   BIMDataSearch
