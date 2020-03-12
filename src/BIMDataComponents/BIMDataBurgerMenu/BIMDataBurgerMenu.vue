3871669da903ff035491a9b7919f45f12b6cc9af
Gaelle <gaelle@bimdata.io>
2020-03-12 10:26:58 +0100

    create a BIMDataBurgerMenucomponent

---
+++ src/BIMDataComponents/BIMDataBurgerMenu/BIMDataBurgerMenu.vue
@@ -0,0 +1,18 @@
+<template>
+  <BIMDataIcon icon-name="search" width="18" height="18" x="23" y="23" class="bimdata-fill-primary">
+    <BIMDataBurgerMenuIcon />
+  </BIMDataIcon>
+</template>
+
+<script>
+/* import BIMData ICONS */
+import BIMDataIcon from "../BIMDataIcons/BIMDataIcon.vue";
+import BIMDataBurgerMenuIcon from "../BIMDataIcons/BIMDataLibraryIcons/BIMDataBurgerMenuIcon";
+
+export default {
+  components: {
+    BIMDataIcon,
+    BIMDataBurgerMenuIcon
+  }
+}
+</script>
