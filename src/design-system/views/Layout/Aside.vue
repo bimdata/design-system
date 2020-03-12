6fde4b9961aaf28f8e734911203f7b6ab393625a
Gaelle <gaelle@bimdata.io>
2020-03-12 11:18:38 +0100

    maj Aside

--- src/design-system/views/Layout/Aside.vue
+++ src/design-system/views/Layout/Aside.vue
@@ -1,10 +1,10 @@
 <template>
   <aside class="aside">
     <div style="flex-grow: 1; overflow-y: auto;">
-      <h2 class="aside__title" @click="onTitleClick">{{ getPathFirstElement() }}</h2>
+      <h4 class="aside__title" @click="onTitleClick">{{ getPathFirstElement() }}</h4>
       <ul class="aside__text bimdata-list">
         <li v-for="child in $store.state[getPathFirstElement()].children" :key="child.id">
-          <router-link :to="child.path">{{ child.title }}</router-link>
+          <router-link :to="{ name: child.path}">{{ child.title }}</router-link>
         </li>
       </ul>
     </div>
