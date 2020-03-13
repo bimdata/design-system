cf2b0897f037776339913d26c0842d87f7be1a61
Gaelle <gaelle@bimdata.io>
2020-03-13 16:23:10 +0100

    create a BIMDataTable component

--- src/BIMDataComponents/BIMDataTable/BIMDataTable.vue
+++ src/BIMDataComponents/BIMDataTable/BIMDataTable.vue
@@ -1,14 +1,56 @@
 <template>
-  <table class="bimdata-table">
-    <thead>
-      <tr>
-        <td></td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <table class="bimdata-table">
+      <thead>
+        <tr>
+          <th v-for="(cell, headerIndex) of header" :key="`header-${headerIndex}-${cell}`">
+            {{ cell }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, rowIndex) of contentRows" :key="`row-${rowIndex}`">
+          <td v-for="(cell, cellIndex) of row" :key="`row-${rowIndex}-cell-${cellIndex}`">
+            {{ cell }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    rows: {
+      validator(rows) {
+        try {
+          const length = rows[0].length;
+          return rows.every(row => row.length === length && row.every(cell => typeof cell === "string" || typeof cell === "number"));
+        } catch(e) {
+            return false;
+        }
+      }
+    }
+  },
+  computed: {
+    header() {
+      return this.rows[0];
+    },
+    contentRows() {
+      return this.rows.slice(1);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  // import BIMDATA VARIABLES
+  @import "../../assets/scss/_BIMDataVariables.scss";
+
+  // import BIMDATA MIXINS
+  @import "../../assets/scss/mixins/_font-size.scss";
+
+  // import BIMDATA STYLE COMPONENT
+  @import "./_BIMDataTable.scss";
+</style>
--- src/design-system/views/views-item/Table/Table.vue
+++ src/design-system/views/views-item/Table/Table.vue
@@ -2,13 +2,45 @@
   <main class="article article-table">
     <div class="article-wrapper">
       <h2> {{ $route.name }} </h2>
+      <Code componentTitle='Table'>
+        <template #module>
+          <BIMDataTable :rows="rowData">
+          </BIMDataTable>
+        </template>
+
+        <template #parameters>
+
+        </template>
+
+        <template #code>
+          <pre>
+            &lt;BIMDataTable :rows="rowData"&gt;
+            &lt;/BIMDataTable&gt;
+          </pre>
+        </template>
+      </Code>
     </div>
   </main>
 </template>
 
 <script>
-export default {
+import Code from "../../Components/Code.vue";
+import BIMDataTable from "../../../../BIMDataComponents/BIMDataTable/BIMDataTable.vue";
 
+export default {
+  components: {
+    Code,
+    BIMDataTable
+  },
+  data() {
+    return {
+      rowData: [
+        ['name', 'entreprise'],
+        ['Jabriel', 'BIMData'],
+        ['Jabriel', 'BIMData']
+      ]
+    }
+  }
 }
 </script>
 
