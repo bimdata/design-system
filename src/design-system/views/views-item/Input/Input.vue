3df96d7b12330f3f24c2d93049836bd339247ac0
Gaelle <gaelle@bimdata.io>
2020-03-18 12:25:29 +0100

    add parameters & doc to input ds

--- src/design-system/views/views-item/Input/Input.vue
+++ src/design-system/views/views-item/Input/Input.vue
@@ -6,26 +6,56 @@
        <Code :componentTitle='$route.name'>
         <template #module >
           <BIMDataInput
-            v-model="testInput"
+            v-model="textInput"
             placeholder="Your placeholder here"
-            :error="!testInput"
-            errorMessage="your error message here"
-            :loading="loading"
+            :error="getError"
+            :success="getSuccess"
+            :errorMessage="getErrorMessage()"
+            :successMessage="getSuccessMessage()"
+            :loading="getLoading()"
+            :disabled="getDisabled()"
           />
         </template>
 
         <template #parameters>
-
+          <BIMDataCheckbox
+            text="error"
+            v-model="error"
+            :disabled="checkboxErrorDisabled"
+          />
+          <BIMDataCheckbox
+            text="success"
+            v-model="success"
+            :disabled="checkboxSuccessDisabled"
+          />
+          <BIMDataCheckbox
+            text="error message"
+            v-model="errorMessage"
+            :disabled="checkboxErrorMessageDisabled"
+          />
+          <BIMDataCheckbox
+            text="success message"
+            v-model="successMessage"
+            :disabled="checkboxSuccessMessageDisabled"
+          />
+          <BIMDataCheckbox
+            text="loading"
+            v-model="loading"
+          />
+          <BIMDataCheckbox
+            text="disabled"
+            v-model="disabled"
+          />
         </template>
 
         <template #code>
           <pre>
             &lt;BIMDataInput
-            v-model="testInput"
+            v-model="textInput"
             placeholder="Your placeholder here"
-            :error="!testInput"
+            :error="!textInput"
             errorMessage="your error message here"
-            :loading="loading"
+            :loading="{{getLoading()}}"
           /&gt;
           </pre>
         </template>
@@ -41,46 +71,111 @@
 
 <script>
 import Code from "../../Components/Code.vue";
-
 import BIMDataTable from "@/BIMDataComponents/BIMDataTable/BIMDataTable.vue";
+import BIMDataCheckbox from "@/BIMDataComponents/BIMDataCheckbox/BIMDataCheckbox.vue";
+
 import BIMDataInput from "@/BIMDataComponents/BIMDataInput/BIMDataInput.vue";
 
 export default {
   components: {
     Code,
     BIMDataTable,
+    BIMDataCheckbox,
     BIMDataInput
   },
   data() {
     return {
-      testInput: "",
+      textInput: "",
+      error: false,
+      success: false,
+      errorMessage: false,
+      successMessage: false,
       loading: false,
+      disabled:false,
+      checkboxErrorDisabled: false,
+      checkboxSuccessDisabled: false,
+      checkboxErrorMessageDisabled: true,
+      checkboxSuccessMessageDisabled: true,
       propsData: [
         [
           "Props", "Type", "Default value", "Description"
         ],
         [
-          "placeholder", "String", "''", ""
+          "placeholder", "String", "' '", "Use this props to add a placeholder to your input."
         ],
         [
-          "error", "Boolean", "false", ""
+          "error", "Boolean", "false", "Use this boolean to check if your input is bad."
         ],
         [
-          "success", "Boolean", "false", ""
+          "success", "Boolean", "false", "Use this boolean to check if your input is good."
         ],
         [
-          "errorMessage", "String", "''", ""
+          "errorMessage", "String", "' '", "Use this props to add an error message to your input."
         ],
         [
-          "successMessage", "String", "''", ""
+          "successMessage", "String", "' '", "Use this props to add a success message to your input."
         ],
         [
           "loading", "Boolean", "false", ""
         ],
         [
-          "disabled", "Boolean", "false", ""
+          "disabled", "Boolean", "false", "Use this boolean to disabled your input."
         ]
       ]
+    }
+  },
+  methods: {
+    getErrorMessage(){
+      if(this.error && this.errorMessage){
+        return "your error message here";
+      }
+    },
+    getSuccessMessage(){
+      if(this.success && this.successMessage){
+        return "your success message here";
+      }
+    },
+    getLoading(){
+      if(this.loading){
+        return true;
+      }else{
+        return false;
+      }
+    },
+    getDisabled(){
+      if(this.disabled){
+        return true;
+      }else{
+        return false;
+      }
+    }
+  },
+  computed: {
+    getError(){
+      if(this.error){
+        this.checkboxSuccessDisabled = true;
+        this.checkboxSuccessMessageDisabled = true;
+        this.checkboxErrorMessageDisabled = false;
+        this.successMessage = false;
+        return !this.textInput;
+      } else {
+        this.checkboxSuccessDisabled = false;
+        this.checkboxErrorMessageDisabled = true;
+        this.errorMessage = false;
+      }
+    },
+    getSuccess(){
+      if(this.success){
+        this.checkboxErrorDisabled = true;
+        this.checkboxErrorMessageDisabled = true;
+        this.checkboxSuccessMessageDisabled = false;
+        this.errorMessage = false;
+        return !this.textInput;
+      } else {
+        this.checkboxErrorDisabled = false;
+        this.checkboxSuccessMessageDisabled = true;
+        this.successMessage = false;
+      }
     }
   }
 }
