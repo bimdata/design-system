b251b1d64da8f0cf67ac0aaf54361e82cf08484f
Gaelle <gaelle@bimdata.io>
2020-03-20 14:10:53 +0100

    use prism for Typography ds

--- src/design-system/views/views-item/Typography/Typography.vue
+++ src/design-system/views/views-item/Typography/Typography.vue
@@ -42,25 +42,44 @@
         <h3>Font weights</h3>
         <div class="typography-weight__content">
           <div class="typography-weight__content__item">
-            <pre><code class="language-scss"><span class="var">$primary-font;</span></code></pre>
+            <pre
+              class="language-scss"
+              v-copy="onCopy"
+            ><code class="language-scss" v-highlight:scss>$primary-font;</code></pre>
             <small>Roboto Regular</small>
           </div>
           <div class="typography-weight__content__item">
-            <pre><code class="language-scss">$primary-font-medium;</code></pre>
+            <pre
+              class="language-scss"
+              v-copy="onCopy"
+            ><code v-highlight:scss class="language-scss">$primary-font-medium;</code></pre>
             <small>Roboto Medium</small>
           </div>
           <div class="typography-weight__content__item">
-            <pre><code class="language-scss">$primary-font-bold;</code></pre>
+            <pre
+              class="language-scss"
+              v-copy="onCopy"
+            ><code class="language-scss" v-highlight:scss>$primary-font-bold;</code></pre>
             <small>Roboto Bold</small>
           </div>
         </div>
       </div>
       <div class="typography-use">
         <h3>How to use</h3>
-        <p>If you use sass preprocessor into your project, you can import font-size mixins for display font-size into rem unity. If not, you can simply use font-size property.</p>
+        <p>
+          If you use sass preprocessor into your project, you can import
+          font-size mixins for display font-size into rem unity. If not, you can
+          simply use font-size property.
+        </p>
         <div class="typography-use__content">
-          <pre><code class="language-css">@import "./assets/scss/mixins/font-size.scss"</code></pre>
-          <pre><code class="language-css">@include font-size(12px);</code></pre>
+          <pre
+            class="language-scss"
+            v-copy="onCopy"
+          ><code class="language-scss" v-highlight:scss>@import "./assets/scss/mixins/font-size.scss"</code></pre>
+          <pre
+            class="language-scss"
+            v-copy="onCopy"
+          ><code class="language-scss" v-highlight:scss>@include font-size(12px);</code></pre>
         </div>
       </div>
     </div>
@@ -68,14 +87,31 @@
 </template>
 
 <script>
+import highlight from "@/directives/highlight.js";
+import copy from "@/directives/copy.js";
+
+import Prism from "prismjs";
 
 export default {
-
-}
+  methods: {
+    onCopy(e) {
+      this.alerts = true;
+      this.message = "copied successfully !";
+      this.alertType = "success";
+      setTimeout(() => {
+        this.alerts = false;
+      }, 3000);
+    }
+  },
+  directives: { highlight, copy }
+};
 </script>
 
 <style lang="scss">
-  @import "@/assets/scss/_BIMDataVariables.scss";
+// import PRISMJS THEME
+@import "../../../../../node_modules/prismjs/themes/prism-tomorrow.css";
 
-  @import "./_Typography.scss";
+@import "@/assets/scss/_BIMDataVariables.scss";
+
+@import "./_Typography.scss";
 </style>
