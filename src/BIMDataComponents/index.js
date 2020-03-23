6c698f1905042edc245ba5d05e757f682b0323e2
Gaelle <gaelle@bimdata.io>
2020-03-23 14:37:28 +0100

    add new components to export
    BIMDataCard, BIMDataInput, BIMDataSelect, BIMDataTable & BIMDataTextarea

--- src/BIMDataComponents/index.js
+++ src/BIMDataComponents/index.js
@@ -1,17 +1,27 @@
 import BIMDataButton from './BIMDataButton/BIMDataButton.vue';
+import BIMDataCard from './BIMDataButton/BIMDataCard.vue';
 import BIMDataCheckbox from './BIMDataCheckbox/BIMDataCheckbox.vue';
 import BIMDataIcon from './BIMDataIcons/BIMDataIcon.vue';
 import BIMDataIcons from './BIMDataIcons/icons.js';
+import BIMDataInput from './BIMDataInput/BIMDataInput.vue';
 import BIMDataRadio from './BIMDataRadio/BIMDataRadio.vue';
 import BIMDataSearch from './BIMDataSearch/BIMDataSearchBar.vue';
+import BIMDataSelect from './BIMDataSelect/BIMDataSelect.vue';
+import BIMDataTable from './BIMDataTable/BIMDataTable.vue';
+import BIMDataTextarea from './BIMDataTextarea/BIMDataTextarea.vue';
 
 const Components = {
   BIMDataButton,
+  BIMDataCard,
   BIMDataCheckbox,
   BIMDataIcon,
   BIMDataIcons,
+  BIMDataInput,
   BIMDataRadio,
-  BIMDataSearch
+  BIMDataSearch,
+  BIMDataSelect,
+  BIMDataTable,
+  BIMDataTextarea
 };
 
 export default Components;
