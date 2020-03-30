b96c6a024b6acec5010e60561b32311a6b6bfefb
Gaelle <gaelle@bimdata.io>
2020-03-30 18:16:59 +0200

    create BIMDataTooltip component

---
+++ src/BIMDataComponents/BIMDataTooltip/BIMDataTooltip.vue
@@ -0,0 +1,28 @@
+<template>
+  <span class="bimdata-tooltip">{{ message }}</span>
+</template>
+
+<script>
+export default {
+  name: "BaseTooltip",
+  props: {
+    message: {
+      type: String,
+      default: null
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+  // import BIMDATA VARIABLES
+  @import "../../assets/scss/_BIMDataVariables.scss";
+
+  // import BIMDATA MIXINS
+  @import "../../assets/scss/mixins/_font-size.scss";
+  @import "../../assets/scss/mixins/_pseudo.scss";
+
+  // import BIMDATA STYLE COMPONENT
+  @import "./_keyframes.scss";
+  @import "./_BIMDataTooltip.scss";
+</style>
