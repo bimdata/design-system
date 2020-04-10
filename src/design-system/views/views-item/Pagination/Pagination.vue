259b2dd1c03b7be2be4e17e3e1dd8b8c4c72bdb1
Gaelle <gaelle@bimdata.io>
2020-04-10 17:03:05 +0200

    import BIMDataPagination into Pagination ds page

--- src/design-system/views/views-item/Pagination/Pagination.vue
+++ src/design-system/views/views-item/Pagination/Pagination.vue
@@ -2,17 +2,38 @@
   <main class="article article-pagination">
     <div class="article-wrapper">
       <h2> {{ $route.name }} </h2>
+      <Code :componentTitle='$route.name'>
+        <template #module >
+          <BIMDataPagination :length="10" :currentPage="1" :totalPages="4" :perPage="20">
+
+          </BIMDataPagination>
+        </template>
+
+        <template #parameters>
+
+        </template>
+
+        <template #code>
+          <pre>
+
+          </pre>
+        </template>
+      </Code>
     </div>
   </main>
 </template>
 
 <script>
-
+import Code from "../../Components/Code.vue";
+import BIMDataPagination from "@/BIMDataComponents/BIMDataPagination/BIMDataPagination.vue"
 export default {
-
+  components: {
+    Code,
+    BIMDataPagination
+  }
 }
 </script>
 
 <style lang="scss">
-
+  @import "@/assets/scss/_BIMDataVariables.scss";
 </style>
