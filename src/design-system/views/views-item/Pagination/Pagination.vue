53e5cb3edbe275091eebc0dbf74225987a49a488
Gaelle <gaelle@bimdata.io>
2020-04-14 14:43:51 +0200

    use BIMDataPaginatedList for pagination ds page

--- src/design-system/views/views-item/Pagination/Pagination.vue
+++ src/design-system/views/views-item/Pagination/Pagination.vue
@@ -4,13 +4,15 @@
       <h2> {{ $route.name }} </h2>
       <Code :componentTitle='$route.name'>
         <template #module >
-          <BIMDataPagination :length="10" :currentPage="1" :totalPages="4" :perPage="20">
+          <BIMDataPaginatedList :list="paginatedListExample" :perPage="Number(textInput)">
 
-          </BIMDataPagination>
+          </BIMDataPaginatedList>
         </template>
 
         <template #parameters>
+          <BIMDataInput v-model="textInput" placeholder="Number of items per page" type="number">
 
+          </BIMDataInput>
         </template>
 
         <template #code>
@@ -25,11 +27,33 @@
 
 <script>
 import Code from "../../Components/Code.vue";
-import BIMDataPagination from "@/BIMDataComponents/BIMDataPagination/BIMDataPagination.vue"
+import BIMDataPaginatedList from "@/BIMDataComponents/BIMDataPaginatedList/BIMDataPaginatedList.vue"
+import BIMDataInput from "@/BIMDataComponents/BIMDataInput/BIMDataInput.vue";
+
 export default {
   components: {
     Code,
-    BIMDataPagination
+    BIMDataPaginatedList,
+    BIMDataInput
+  },
+  data(){
+    return{
+      textInput: 4,
+      paginatedListExample: [
+        "item 01",
+        "item 02",
+        "item 03",
+        "item 04",
+        "item 05",
+        "item 06",
+        "item 07",
+        "item 08",
+        "item 09",
+        "item 10",
+        "item 11",
+        "item 12"
+      ]
+    }
   }
 }
 </script>
