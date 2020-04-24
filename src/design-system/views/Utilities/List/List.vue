afde962b5ec924ecd149a777275979d89ad84fe5
Gaelle <gaelle@bimdata.io>
2020-04-24 15:36:51 +0200

    doc list utilities

--- src/design-system/views/Utilities/List/List.vue
+++ src/design-system/views/Utilities/List/List.vue
@@ -1,10 +1,18 @@
 <template>
-  <main class="article article-list">
+  <main class="article utilities-list">
     <div class="article-wrapper">
       <h2> {{ $route.name }} </h2>
+      <p>List utility classes will help you to reset list found within BIMData projects.</p>
       <Code language="xml">
-        &lt;div class="bimdata-list"&gt;&lt;/div&gt;
+        &lt;ul class="bimdata-list"&gt;&lt;/ul&gt;
       </Code>
+      <h3>Examples</h3>
+      <div class="bimdata-ds__example">
+        <ul class="bimdata-list">
+          <li>item 01</li>
+          <li>item 02</li>
+        </ul>
+      </div>
     </div>
   </main>
 </template>
