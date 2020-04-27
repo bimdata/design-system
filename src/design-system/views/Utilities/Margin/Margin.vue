48d9ab4989708a6ca18e155c69920e774cd2b9fb
Gaelle <gaelle@bimdata.io>
2020-04-27 15:52:01 +0200

    delete comment code and add global, vertical, horizontal margin

--- src/design-system/views/Utilities/Margin/Margin.vue
+++ src/design-system/views/Utilities/Margin/Margin.vue
@@ -3,15 +3,11 @@
     <div class="article-wrapper">
       <h2> {{ $route.name }} </h2>
       <p>All spacing is defined by a base value of 12 pixels. This value of 12 px is the basic unit of measurement for spacing.</p>
-      <!-- <Code language="xml">
-        &lt;div class="m-t-6"&gt;&lt;/div&gt;
-      </Code>
-      <h3>Examples</h3> -->
       <ComponentCode :componentTitle="$route.name">
         <template #module>
           <div class="ds-spacing">
             <div :class="getMarginClass()">
-              <span>margin top 6px</span>
+              <span>{{getMarginText()}}</span>
             </div>
           </div>
         </template>
@@ -22,13 +18,18 @@
               :text="checkbox.text"
               v-model="checkbox.checked"
               :key="checkbox.text"
+              class="m-y-6"
             >
             </BIMDataCheckbox>
           </template>
         </template>
 
         <template #code>
-            <pre></pre>
+          <pre>
+            &lt;div class="{{getMarginText()}}"&gt;
+              content here
+            &lt;/div&gt;
+          </pre>
         </template>
       </ComponentCode>
     </div>
@@ -39,7 +40,6 @@
 import ComponentCode from "@/design-system/views/Elements/ComponentCode/ComponentCode.vue";
 import BIMDataCheckbox from "@/BIMDataComponents/BIMDataCheckbox/BIMDataCheckbox.vue";
 
-
 export default {
   components: {
     ComponentCode,
@@ -49,52 +49,74 @@ export default {
     return {
       checkboxes: [
         {
+          class: "m-6",
+          checked: false,
+          text: "global 6px margin"
+        },
+        {
+          class: "m-x-6",
+          checked: false,
+          text: "horizontal 6px margin"
+        },
+        {
+          class: "m-y-6",
+          checked: false,
+          text: "vertical 6px margin"
+        },
+        {
           class: "m-t-6",
           checked: false,
-          text: "margin top 6px",
-          disabled: false
+          text: "margin top 6px"
         },
         {
           class: "m-b-6",
           checked: false,
-          text: "margin bottom 6px",
-          disabled: false
+          text: "margin bottom 6px"
         },
         {
           class: "m-l-6",
           checked: false,
-          text: "margin left 6px",
-          disabled: false
+          text: "margin left 6px"
         },
         {
           class: "m-r-6",
           checked: false,
-          text: "margin right 6px",
-          disabled: false
+          text: "margin right 6px"
+        },
+        {
+          class: "m-12",
+          checked: false,
+          text: "global 12px margin"
+        },
+        {
+          class: "m-x-12",
+          checked: false,
+          text: "horizontal 12px margin"
+        },
+        {
+          class: "m-y-12",
+          checked: false,
+          text: "vertical 12px margin"
         },
         {
           class: "m-t-12",
           checked: false,
-          text: "margin top 12px",
-          disabled: false
+          text: "margin top 12px"
         },
         {
           class: "m-b-12",
           checked: false,
-          text: "margin bottom 12px",
-          disabled: false
+          text: "margin bottom 12px"
         },
         {
           class: "m-l-12",
           checked: false,
-          text: "margin left 12px",
-          disabled: false
+          text: "margin left 12px"
         },
         {
           class: "m-r-12",
           checked: false,
-          text: "margin right 12px",
-          disabled: false
+          text: "margin right 12px"
         }
       ],
     }
@@ -102,6 +124,9 @@ export default {
   methods: {
     getMarginClass() {
       return this.checkboxes.filter(checkbox => checkbox.checked).map(checkbox => checkbox.class);
+    },
+    getMarginText() {
+      return this.getMarginClass().join(" ");
     }
   }
 }
