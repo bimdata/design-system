c43ca6965e435c63b3c240a7a236f12c37735325
Gaelle <gaelle@bimdata.io>
2020-04-27 15:52:22 +0200

    add padding into utilities

--- src/design-system/views/Utilities/Padding/Padding.vue
+++ src/design-system/views/Utilities/Padding/Padding.vue
@@ -3,109 +3,139 @@
     <div class="article-wrapper">
       <h2> {{ $route.name }} </h2>
       <p>All spacing is defined by a base value of 12 pixels. This value of 12 px is the basic unit of measurement for spacing.</p>
-      <Code language="xml">
-        &lt;div class="p-t-6"&gt;&lt;/div&gt;
-      </Code>
-      <h3>Examples</h3>
-      <div class="flex">
-        <div class="m-r-12">
-          <h6>Margin - top</h6>
-          <div class="bimdata-ds__example">
-            <div class="ds-spacing">
-              <div class="m-t-6">
-                <span>margin top 6px</span>
-              </div>
+      <ComponentCode :componentTitle="$route.name">
+        <template #module>
+          <div class="ds-spacing">
+            <div class="ds-spacing__padding" :class="getPaddingClass()">
+              <span>{{getPaddingText()}}</span>
             </div>
           </div>
-        </div>
-        <div class="m-r-12">
-          <h6>Margin - bottom</h6>
-          <div class="bimdata-ds__example">
-            <div class="ds-spacing">
-              <div class="m-b-6">
-                <span>margin bottom 6px</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="m-r-12">
-          <h6>Margin - left</h6>
-          <div class="bimdata-ds__example">
-            <div class="ds-spacing">
-              <div class="m-l-6">
-                <span>margin left 6px</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="m-r-12">
-          <h6>Margin - right</h6>
-          <div class="bimdata-ds__example">
-            <div class="ds-spacing">
-              <div class="m-r-6">
-                <span>margin right 6px</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="flex">
-        <div class="m-r-12">
-          <h6>Margin - top</h6>
-          <div class="bimdata-ds__example">
-            <div class="ds-spacing">
-              <div class="m-t-12">
-                <span>margin top 12px</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="m-r-12">
-          <h6>Margin - bottom</h6>
-          <div class="bimdata-ds__example">
-            <div class="ds-spacing">
-              <div class="m-b-12">
-                <span>margin bottom 12px</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="m-r-12">
-          <h6>Margin - left</h6>
-          <div class="bimdata-ds__example">
-            <div class="ds-spacing">
-              <div class="m-l-12">
-                <span>margin left 12px</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="m-r-12">
-          <h6>Margin - right</h6>
-          <div class="bimdata-ds__example">
-            <div class="ds-spacing">
-              <div class="m-r-12">
-                <span>margin right 12px</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        </template>
+
+        <template #parameters>
+          <template v-for="checkbox of checkboxes">
+            <BIMDataCheckbox
+              :text="checkbox.text"
+              v-model="checkbox.checked"
+              :key="checkbox.text"
+              class="m-y-6"
+            >
+            </BIMDataCheckbox>
+          </template>
+        </template>
+
+        <template #code>
+          <pre>
+            &lt;div class="{{getPaddingText()}}"&gt;
+              content here
+            &lt;/div&gt;
+          </pre>
+        </template>
+      </ComponentCode>
     </div>
   </main>
 </template>
 
 <script>
-import Code from "../../Elements/Code/Code.vue";
+import ComponentCode from "@/design-system/views/Elements/ComponentCode/ComponentCode.vue";
+import BIMDataCheckbox from "@/BIMDataComponents/BIMDataCheckbox/BIMDataCheckbox.vue";
+
 export default {
   components: {
-    Code
+    ComponentCode,
+    BIMDataCheckbox
+  },
+  data() {
+    return {
+      checkboxes: [
+        {
+          class: "p-6",
+          checked: false,
+          text: "global 6px padding"
+        },
+        {
+          class: "p-x-6",
+          checked: false,
+          text: "horizontal 6px padding"
+        },
+        {
+          class: "p-y-6",
+          checked: false,
+          text: "vertical 6px padding"
+        },
+        {
+          class: "p-t-6",
+          checked: false,
+          text: "padding top 6px"
+        },
+        {
+          class: "p-b-6",
+          checked: false,
+          text: "padding bottom 6px"
+        },
+        {
+          class: "p-l-6",
+          checked: false,
+          text: "padding left 6px"
+        },
+        {
+          class: "p-r-6",
+          checked: false,
+          text: "padding right 6px"
+        },
+        {
+          class: "p-12",
+          checked: false,
+          text: "global 12px padding"
+        },
+        {
+          class: "p-x-12",
+          checked: false,
+          text: "horizontal 12px padding"
+        },
+        {
+          class: "p-y-12",
+          checked: false,
+          text: "vertical 12px padding"
+        },
+        {
+          class: "p-t-12",
+          checked: false,
+          text: "padding top 12px"
+        },
+        {
+          class: "p-b-12",
+          checked: false,
+          text: "padding bottom 12px"
+        },
+        {
+          class: "p-l-12",
+          checked: false,
+          text: "padding left 12px"
+        },
+        {
+          class: "p-r-12",
+          checked: false,
+          text: "padding right 12px"
+        }
+      ],
+    }
+  },
+  methods: {
+    getPaddingClass() {
+      return this.checkboxes.filter(checkbox => checkbox.checked).map(checkbox => checkbox.class);
+    },
+    getPaddingText() {
+      return this.getPaddingClass().join(" ");
+    }
   }
 }
 </script>
 
 <style lang="scss">
   @import "@/assets/scss/_BIMDataVariables.scss";
+  
+  @import "@/assets/scss/mixins/_font-size.scss";
 
   @import "./_Padding.scss";
 </style>
