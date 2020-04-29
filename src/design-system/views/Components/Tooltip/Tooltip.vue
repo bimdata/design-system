c03994af1a09f924c2230005330c7eb8d9855662
Gaelle <gaelle@bimdata.io>
2020-04-29 18:02:22 +0200

    add parameters to tooltip ds page

--- src/design-system/views/Components/Tooltip/Tooltip.vue
+++ src/design-system/views/Components/Tooltip/Tooltip.vue
@@ -4,9 +4,36 @@
       <h2> {{ $route.name }} </h2>
       <ComponentCode :componentTitle="$route.name">
         <template #module>
-          <BIMDataTooltip message="tooltip here" className="bimdata-tooltip--left">
-            <template #content>Hover me</template>
+          <BIMDataTooltip message="tooltip here" :className="getOverviewTooltipClasses()">
+            <template #content><span>Hover me</span></template>
           </BIMDataTooltip>
+        </template>
+
+        <template #parameters>
+          <div
+            class="bimdata-ds__demo__parameters__options"
+            v-for="[key, values] in Object.entries(tooltipOptions)"
+            :key="key"
+          >
+            <h5>{{ key }}</h5>
+            <BIMDataRadio
+              v-for="value in values"
+              :key="value"
+              :text="value"
+              :id="value"
+              :value="value"
+              :name="key"
+              v-model="$data[`selectedTooltipOptions${key}`]"
+            >
+            </BIMDataRadio>
+          </div>
+          <div class="bimdata-ds__demo__parameters__options">
+              <h5>modifiers</h5>
+            <BIMDataCheckbox
+              text="arrow"
+              v-model="checkboxArrowChecked">
+            </BIMDataCheckbox>
+          </div>
         </template>
 
         <template #code>
@@ -22,12 +49,36 @@
 <script>
 import ComponentCode from "../../Elements/ComponentCode/ComponentCode.vue";
 import BIMDataTooltip from "@/BIMDataComponents/BIMDataTooltip/BIMDataTooltip.vue";
-
+import BIMDataRadio from "@/BIMDataComponents/BIMDataRadio/BIMDataRadio.vue";
+import BIMDataCheckbox from "@/BIMDataComponents/BIMDataCheckbox/BIMDataCheckbox.vue";
 
 export default {
   components: {
     ComponentCode,
-    BIMDataTooltip
+    BIMDataTooltip,
+    BIMDataRadio,
+    BIMDataCheckbox
+  },
+  data(){
+    return {
+      selectedTooltipOptionsposition: "bottom",
+      selectedTooltipOptionscolor: "primary",
+      checkboxArrowChecked: false,
+      tooltipOptions: {
+        position: ["left", "right", "bottom", "up"],
+        color: ["primary", "grey"]
+      }
+    }
+  },
+
+  methods: {
+    getOverviewTooltipClasses() {
+      if (this.checkboxArrowChecked) {
+        return `bimdata-tooltip--${this.selectedTooltipOptionsposition} bimdata-tooltip--${this.selectedTooltipOptionscolor} bimdata-tooltip--arrow`
+      } else {
+        return `bimdata-tooltip--${this.selectedTooltipOptionsposition} bimdata-tooltip--${this.selectedTooltipOptionscolor}`
+      }
+    }
   }
 }
 </script>
