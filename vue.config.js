7f6a549249cb295c3e7fb9f72c9a68d8b1ab6522
Gaelle <gaelle@bimdata.io>
2020-05-27 18:20:39 +0200

    resolve require for img

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   productionSourceMap: false,
+  chainWebpack: config => {
+    config.module.rule('css')
+      .test(/\.css$/)
+      .use('url-loader')
+        .loader('url-loader')
+        .options({
+          esModule: false,
+        })
+        .end()
+  }
 };
