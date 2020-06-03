ac7e9ec489d96d5494c75dceb747da28f5f74666
Antoine Mineau <antoine@bimdata.io>
2020-06-03 16:48:33 +0200

    update rollup

--- postBuild.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cp -a src/assets/fonts/ dist/fonts
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { terser } from "rollup-plugin-terser";
 import vue from "rollup-plugin-vue";
 import css from "rollup-plugin-css-only";
+import copy from "rollup-plugin-copy";
 
 export default {
   input: "src/BIMDataComponents/index.js",
@@ -13,12 +14,16 @@ export default {
       name: "design-system",
       file: "dist/design-system.js",
       format: "umd",
-    }
+    },
   ],
   plugins: [
+    copy({
+      targets: [{ src: "public/*", dest: "dist" }],
+      targets: [{ src: "src/assets/fonts", dest: "dist/fonts" }],
+    }),
     vue({ template: { isProduction: true } }),
     css({
-      output: 'dist/css/design-system.css'
+      output: "dist/css/design-system.css",
     }),
     terser(),
   ],
