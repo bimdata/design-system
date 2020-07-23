b5c25100c07f84ece767769ba925945e8264a285
Gaelle <gaelle@bimdata.io>
2020-07-23 16:15:23 +0200

    change icons files

---
+++ src/BIMDataComponents/BIMDataIcons/BIMDataLibraryIcons/index.js
@@ -0,0 +1,125 @@
+import addFile from "./AddFile.vue";
+import addFolder from "./AddFolder.vue";
+import addUser from "./AddUser.vue";
+import api from "./Api.vue";
+import arrow from "./Arrow.vue";
+import attach from "./Attach.vue";
+import bcf from "./Bcf.vue";
+import building from "./Building.vue";
+import burgerMenu from "./BurgerMenu.vue";
+import camera from "./Camera.vue";
+import check from "./Check.vue";
+import chevron from "./Chevron.vue";
+import click from "./Click.vue";
+import close from "./Close.vue";
+import cloud from "./Cloud.vue";
+import color from "./Color.vue";
+import cursor from "./Cursor.vue";
+import Default from "./Default.vue";
+import Delete from "./Delete.vue";
+import deployDown from "./DeployDown.vue";
+import deployUp from "./DeployUp.vue";
+import detach from "./Detach.vue";
+import doubleChevron from "./DoubleChevron.vue";
+import edit from "./Edit.vue";
+import exportIfc from "./ExportIfc.vue";
+import failed from "./Failed.vue";
+import filter from "./Filter.vue";
+import folder from "./Folder.vue";
+import folderOpen from "./FolderOpen.vue";
+import fullscreen from "./Fullscreen.vue";
+import hide from "./Hide.vue";
+import ifc from "./Ifc.vue";
+import ifcFile from "./IfcFile.vue";
+import Import from "./Import.vue";
+import information from "./Information.vue";
+import isolate from "./Isolate.vue";
+import listManage from "./ListManage.vue";
+import location from "./Location.vue";
+import measure from "./Measure.vue";
+import minus from "./Minus.vue";
+import model3d from "./Model3d.vue";
+import plus from "./Plus.vue";
+import project from "./Project.vue";
+import refresh from "./Refresh.vue";
+import reprocessIfc from "./ReprocessIfc.vue";
+import rules from "./Rules.vue";
+import sandglass from "./Sandglass.vue";
+import search from "./Search.vue";
+import section from "./Section.vue";
+import settings from "./Settings.vue";
+import show from "./Show.vue";
+import showSelected from "./ShowSelected.vue";
+import socotec from "./Socotec.vue";
+import storey from "./Storey.vue";
+import success from "./Success.vue";
+import tree from "./Tree.vue";
+import union from "./Union.vue";
+import user from "./User.vue";
+import validate from "./Validate.vue";
+import warning from "./Warning.vue";
+import windowed from "./Windowed.vue";
+
+export default {
+  addFile,
+  addFolder,
+  addUser,
+  api,
+  arrow,
+  attach,
+  bcf,
+  building,
+  burgerMenu,
+  camera,
+  check,
+  chevron,
+  click,
+  close,
+  cloud,
+  color,
+  cursor,
+  default: Default,
+  delete: Delete,
+  deployDown,
+  deployUp,
+  detach,
+  doubleChevron,
+  edit,
+  exportIfc,
+  failed,
+  filter,
+  folder,
+  folderOpen,
+  fullscreen,
+  hide,
+  ifc,
+  ifcFile,
+  import: Import,
+  information,
+  isolate,
+  listManage,
+  location,
+  measure,
+  minus,
+  model3d,
+  plus,
+  project,
+  refresh,
+  reprocessIfc,
+  rules,
+  sandglass,
+  search,
+  section,
+  settings,
+  show,
+  showSelected,
+  socotec,
+  storey,
+  success,
+  tree,
+  union,
+  user,
+  validate,
+  warning,
+  windowed
+};
