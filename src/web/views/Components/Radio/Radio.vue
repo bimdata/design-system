75a398e133fd4cec14c92a2460d1f990deb630dc
Gaelle <gaelle@bimdata.io>
2020-08-05 16:55:16 +0200

    add codepen and github link

--- src/web/views/Components/Radio/Radio.vue
+++ src/web/views/Components/Radio/Radio.vue
@@ -3,7 +3,7 @@
     <div class="article-wrapper">
       <h2 class="bimdata-h2">{{ $route.name }}</h2>
 
-      <ComponentCode :componentTitle="$route.name" language="javascript">
+      <ComponentCode :componentTitle="$route.name" language="javascript" codepenLink="https://codepen.io/bimdata/pen/MWyWQep" githubLink="https://github.com/bimdata/design-system/blob/develop/src/BIMDataComponents/BIMDataRadio/BIMDataRadio.vue">
         <template #module>
           <BIMDataRadio
             text="click on me !"
