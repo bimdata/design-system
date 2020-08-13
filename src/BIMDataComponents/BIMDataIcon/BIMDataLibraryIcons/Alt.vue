9560766eec47a20d5796208387ba1fc519c664bd
Gaelle <gaelle@bimdata.io>
2020-08-13 16:55:58 +0200

    add some *lost* icons

---
+++ src/BIMDataComponents/BIMDataIcon/BIMDataLibraryIcons/Alt.vue
@@ -0,0 +1,5 @@
+<template>
+  <g>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M11.7015 3.57515L5.95106 8.45858L0.201477 13.342H5.54916V19.4455H17.8546V13.342H23.2015L17.4519 8.45858L11.7015 3.57515Z"/>
+  </g>
+</template>
--- src/BIMDataComponents/BIMDataIcon/BIMDataLibraryIcons/index.js
+++ src/BIMDataComponents/BIMDataIcon/BIMDataLibraryIcons/index.js
@@ -1,7 +1,7 @@
 import addFile from "./AddFile.vue";
 import addFolder from "./AddFolder.vue";
 import addUser from "./AddUser.vue";
-// import alt from "./Alt.vue";
+import alt from "./Alt.vue";
 import api from "./Api.vue";
 import arrow from "./Arrow.vue";
 import attach from "./Attach.vue";
@@ -9,11 +9,11 @@ import bcf from "./Bcf.vue";
 import building from "./Building.vue";
 import burgerMenu from "./BurgerMenu.vue";
 import camera from "./Camera.vue";
-// import cancelCamera from "./CancelCamera.vue";
+import cancelCamera from "./CancelCamera.vue";
 import check from "./Check.vue";
 import chevron from "./Chevron.vue";
-// import classificationCategorie from "./ClassificationCategorie.vue";
-// import classificationElement from "./ClassificationElement.vue";
+import classificationCategorie from "./ClassificationCategorie.vue";
+import classificationElement from "./ClassificationElement.vue";
 import click from "./Click.vue";
 import close from "./Close.vue";
 import cloud from "./Cloud.vue";
@@ -21,11 +21,11 @@ import color from "./Color.vue";
 import cursor from "./Cursor.vue";
 import Default from "./Default.vue";
 import Delete from "./Delete.vue";
-// import deploy from "./Deploy.vue";
+import deploy from "./Deploy.vue";
 import detach from "./Detach.vue";
 import doubleChevron from "./DoubleChevron.vue";
 import edit from "./Edit.vue";
-// import Export from "./Export.vue";
+import Export from "./Export.vue";
 import exportIfc from "./ExportIfc.vue";
 import failed from "./Failed.vue";
 import filter from "./Filter.vue";
@@ -45,7 +45,7 @@ import minus from "./Minus.vue";
 import model3d from "./Model3d.vue";
 import plus from "./Plus.vue";
 import project from "./Project.vue";
-// import quickSelect from "./QuickSelect.vue";
+import quickSelect from "./QuickSelect.vue";
 import refresh from "./Refresh.vue";
 import reprocessIfc from "./ReprocessIfc.vue";
 import rules from "./Rules.vue";
@@ -56,24 +56,24 @@ import settings from "./Settings.vue";
 import show from "./Show.vue";
 import showSelected from "./ShowSelected.vue";
 import socotec from "./Socotec.vue";
-// import space from "./Space.vue";
+import space from "./Space.vue";
 import storey from "./Storey.vue";
 import success from "./Success.vue";
-// import system from "./System.vue";
+import system from "./System.vue";
 import tree from "./Tree.vue";
 import union from "./Union.vue";
 import user from "./User.vue";
 import validate from "./Validate.vue";
-// import video from "./Video.vue";
+import video from "./Video.vue";
 import warning from "./Warning.vue";
 import windowed from "./Windowed.vue";
-// import zone from "./Zone.vue";
+import zone from "./Zone.vue";
 
 export default {
   addFile,
   addFolder,
   addUser,
-  // alt,
+  alt,
   api,
   arrow,
   attach,
@@ -81,11 +81,11 @@ export default {
   building,
   burgerMenu,
   camera,
-  // cancelCamera,
+  cancelCamera,
   check,
   chevron,
-  // classificationCategorie,
-  // classificationElement,
+  classificationCategorie,
+  classificationElement,
   click,
   close,
   cloud,
@@ -93,11 +93,11 @@ export default {
   cursor,
   default: Default,
   delete: Delete,
-  // deploy,
+  deploy,
   detach,
   doubleChevron,
   edit,
-  // export: Export,
+  export: Export,
   exportIfc,
   failed,
   filter,
@@ -117,7 +117,7 @@ export default {
   model3d,
   plus,
   project,
-  // quickSelect,
+  quickSelect,
   refresh,
   reprocessIfc,
   rules,
@@ -128,16 +128,16 @@ export default {
   show,
   showSelected,
   socotec,
-  // space,
+  space,
   storey,
   success,
-  // system,
+  system,
   tree,
   union,
   user,
   validate,
-  // video,
+  video,
   warning,
   windowed,
-  // zone
+  zone
 };
