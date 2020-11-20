7aa5bf5b0fc2ab341d3079d4584cc10883a9cf13
Gaelle <gaelle@bimdata.io>
2020-11-20 15:57:28 +0100

    add BIMDataDropdownList to index.js

--- src/BIMDataComponents/index.js
+++ src/BIMDataComponents/index.js
@@ -10,6 +10,7 @@ export { default as BIMDataBigSpinner } from "./BIMDataBigSpinner/BIMDataBigSpin
 export { default as BIMDataButton } from "./BIMDataButton/BIMDataButton.vue";
 export { default as BIMDataCard } from "./BIMDataCard/BIMDataCard.vue";
 export { default as BIMDataCheckbox } from "./BIMDataCheckbox/BIMDataCheckbox.vue";
+export { default as BIMDataDropdownList } from "./BIMDataDropdownList/BIMDataDropdownList.vue";
 export { default as BIMDataIcon } from "./BIMDataIcon/BIMDataIcon.vue";
 export { default as BIMDataIllustration } from "./BIMDataIllustration/BIMDataIllustration.vue";
 export { default as BIMDataInput } from "./BIMDataInput/BIMDataInput.vue";
