ad8446acb7e693cf9ca2c27afb927c390d183568
Gaelle <gaelle@bimdata.io>
2021-11-26 09:57:24 +0100

    add illustrations doc

---
+++ src/web/views/Components/Illustrations/Illustrations.js
@@ -0,0 +1,57 @@
+export default Object.freeze([
+  {
+    name: "click",
+    x: "23",
+    y: "30",
+  },
+  {
+    name: "emptyBcf",
+    x: "90",
+    y: "90",
+  },
+  {
+    name: "emptyBucket",
+    x: "90",
+    y: "90",
+  },
+  {
+    name: "emptyProperties",
+    x: "90",
+    y: "90",
+  },
+  {
+    name: "emptySectionPlanes",
+    x: "90",
+    y: "90",
+  },
+  {
+    name: "emptyTree",
+    x: "90",
+    y: "90",
+  },
+  {
+    name: "logo",
+    x: "90",
+    y: "90",
+  },
+  {
+    name: "shortcutOpen",
+    x: "200",
+    y: "200",
+  },
+  {
+    name: "shortcutRotate",
+    x: "200",
+    y: "200",
+  },
+  {
+    name: "shortcutZoom",
+    x: "200",
+    y: "200",
+  },
+  {
+    name: "successSplit",
+    x: "200",
+    y: "200",
+  },
+]);
