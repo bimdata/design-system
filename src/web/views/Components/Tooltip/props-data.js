251e860d7a3ac4bb90d00fe012d673eafa09e6a5
NicolasRichel <nicolasrichel.github@gmail.com>
2022-03-14 15:36:58 +0100

    MINOR: feat(components): rewrite bimdata-tooltip component (#177)
    * feat(components): rewrite bimdata-tooltip component
    
    * feat(tooltip): add 'success', 'warning' & 'high' colors
    
    * fix: add retro-compatibility with old tooltip impl
    
    Co-authored-by: NicolasRichel <nicolas@bimdata.io>

---
+++ src/web/views/Components/Tooltip/props-data.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+export default [
+  ["Props", "Type", "Default value", "Required", "Description"],
+  [
+    "text",
+    "String",
+    "",
+    "",
+    "Set tooltip text.",
+  ],
+  [
+    "maxWidth",
+    "String",
+    "240px",
+    "",
+    "Set the maximum with of the tooltip."
+  ],
+  [
+    "position",
+    "String (Enum)",
+    "bottom",
+    "true",
+    "Set tooltip postion. Must be one of: 'top', 'right', 'bottom', 'left'",
+  ],
+  [
+    "color",
+    "String (Enum)",
+    "primary",
+    "true",
+    "Set tooltip color. Must be one of: 'white', 'primary', 'secondary', 'granite-light', 'silver-light'",
+  ],
+  [
+    "disabled",
+    "Boolean",
+    "false",
+    "",
+    "If true, the tooltip will never be shown."
+  ]
+];
