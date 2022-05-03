29c2aedc3a71c37915f700a7678600ce2ba657b7
Paul Vilalta <vilaltapaul@gmail.com>
2022-05-03 18:26:45 +0200

    new logos for versioning

---
+++ src/BIMDataComponents/BIMDataIcon/BIMDataLibraryIcons/Versioning.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg
+    width="23"
+    height="23"
+    viewBox="0 0 23 23"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect x="10.7764" y="3" width="12.2237" height="18" />
+    <rect x="5.38818" y="4.89471" width="3.86011" height="14.2105" />
+    <rect y="6.78949" width="3.86011" height="10.4211" />
+  </svg>
+</template>
