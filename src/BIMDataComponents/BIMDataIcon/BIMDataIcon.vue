1de54a1863791806eff56f78f5bc7e21526a6fe1
Paul Vilalta <64323277+pvilalta@users.noreply.github.com>
2022-05-24 08:53:17 +0200

    PATCH: icon / set viewBox to 23 (formerly 24) (#206)

--- src/BIMDataComponents/BIMDataIcon/BIMDataIcon.vue
+++ src/BIMDataComponents/BIMDataIcon/BIMDataIcon.vue
@@ -2,7 +2,7 @@
   <svg
     xmlns="http://www.w3.org/2000/svg"
     preserveAspectRatio="xMidYMid meet"
-    viewBox="0 0 24 24"
+    viewBox="0 0 23 23"
     width="100%"
     :fill="fillColor"
     :color="color"
