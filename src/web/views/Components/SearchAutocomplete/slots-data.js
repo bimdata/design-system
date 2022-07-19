6d9ace6cfb5c730938ddc52a520b0f7e45736218
Gaelle <gaelle@bimdata.io>
2022-07-19 13:45:54 +0200

    change slots documentation for SearchAutocomplete

--- src/web/views/Components/SearchAutocomplete/slots-data.js
+++ src/web/views/Components/SearchAutocomplete/slots-data.js
@@ -7,6 +7,10 @@ export default [
   ],
   [
     "left",
-    "Use this slot to custom left content beside title and description of research item",
+    "Use this slot to custom left content beside title and description of research item.",
   ],
+  [
+    "logoPlaceholder",
+    "Use this slot when the result has no logo defined. By default, an icon is displayed."
+  ]
 ];
