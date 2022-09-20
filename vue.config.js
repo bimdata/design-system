adb533e285faf0e33c69c68f9b2a17b0b75566b4
Nicolas Richel <nicolasrichel.github@gmail.com>
2022-09-20 14:55:20 +0200

    chore: update dependencies (#233)
    Co-authored-by: NicolasRichel <nicolas@bimdata.io>

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,13 @@
 module.exports = {
   productionSourceMap: false,
   outputDir: "dist-web",
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: '@import "~@/assets/scss/BIMData.scss";',
-      },
-    },
-  },
   chainWebpack: config => {
     config.module
       .rule("css")
       .test(/\.css$/)
       .use("url-loader")
       .loader("url-loader")
-      .options({
-        esModule: false,
-      })
+      .options({ esModule: false })
       .end();
   },
 };
