8d36f2debb3f6199212d1dfe6fd8ada8b971752e
Gaelle <gaelle@bimdata.io>
2022-12-14 17:56:22 +0100

    add slots & props documentation for basic BIMDataMenu

--- src/web/views/Components/Menu/Menus.vue
+++ src/web/views/Components/Menu/Menus.vue
@@ -7,21 +7,50 @@
 
       <MenuInline />
       <BasicMenu />
+      <div class="m-t-12">
+        <BIMDataText component="h5" color="color-primary" margin="15px 0 0">
+          Props:
+        </BIMDataText>
+        <BIMDataTable
+          :columns="propsBasicMenu[0]"
+          :rows="propsBasicMenu.slice(1)"
+        />
+      </div>
+      <div class="m-t-12">
+        <BIMDataText component="h5" color="color-primary" margin="15px 0 0">
+          Slots:
+        </BIMDataText>
+        <BIMDataTable
+          :columns="slotsBasicMenu[0]"
+          :rows="slotsBasicMenu.slice(1)"
+        />
+      </div>
     </div>
   </main>
 </template>
 
 <script>
+import BIMDataTable from "../../../../BIMDataComponents/BIMDataTable/BIMDataTable.vue";
 import BIMDataText from "../../../../BIMDataComponents/BIMDataText/BIMDataText.vue";
 
 import MenuInline from "./MenuInline.vue";
 import BasicMenu from "./BasicMenu.vue";
 
+import propsBasicMenu from "./props-basic-menu";
+import slotsBasicMenu from "./slots-basic-menu";
+
 export default {
   components: {
+    BIMDataTable,
     BIMDataText,
     MenuInline,
     BasicMenu,
+  },
+  data() {
+    return {
+      propsBasicMenu,
+      slotsBasicMenu,
+    };
   },
 };
 </script>
