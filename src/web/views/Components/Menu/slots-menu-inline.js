56b341ed6e2f39b238c7e5f183c15ad23837c9de
Gaelle <gaelle@bimdata.io>
2022-12-20 10:18:47 +0100

    replace "for" by "to"

--- src/web/views/Components/Menu/slots-menu-inline.js
+++ src/web/views/Components/Menu/slots-menu-inline.js
@@ -1,5 +1,5 @@
 export default [
   ["Slot name", "Description"],
   ["button", "Use this slot for custom button icon to open the menu"],
-  ["submenu", "Use this slot for add a submenu"],
+  ["submenu", "Use this slot to add a submenu"],
 ];
