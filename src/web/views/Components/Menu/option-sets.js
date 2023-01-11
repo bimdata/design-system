b05335b31d69a54d3ea4d85977eab720171744f4
Paul Vilalta <vilaltapaul@gmail.com>
2023-01-11 13:39:51 +0100

    add key prop

--- src/web/views/Components/Menu/option-sets.js
+++ src/web/views/Components/Menu/option-sets.js
@@ -1,9 +1,10 @@
 const basicOptions = [
-  { text: "item 01", action: () => console.log("parent clicked") },
-  { text: "item 02" },
-  { text: "item 03" },
-  { text: "item 04" },
+  { key: 1, text: "item 01", action: () => console.log("parent clicked") },
+  { key: 2, text: "item 02" },
+  { key: 3, text: "item 03" },
+  { key: 4, text: "item 04" },
   {
+    key: 5,
     text: "item 05",
     color: "var(--color-high)",
     background: "var(--color-high-lighter)",
