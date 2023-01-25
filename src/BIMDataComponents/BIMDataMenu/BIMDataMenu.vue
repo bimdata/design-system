3de4aceef5c86f6281e4bc07c0464f17679c66c5
Gaelle <gaelle@bimdata.io>
2023-01-25 11:05:59 +0100

    add slot for custom BIMDataMenu

--- src/BIMDataComponents/BIMDataMenu/BIMDataMenu.vue
+++ src/BIMDataComponents/BIMDataMenu/BIMDataMenu.vue
@@ -7,90 +7,102 @@
     }"
     v-clickaway="away"
   >
-    <div
-      :ref="`item-${item.key}`"
-      v-for="item in menuItems"
-      :key="item.key"
-      class="bimdata-menu__item flex items-center"
-      :class="[
-        item.divider ? 'bimdata-menu__item--divider' : '',
-        { hover: isItemHover && currentItemKey === item.key },
-        hasNoChildren(item) ? 'bimdata-menu__item--no-children' : '',
-      ]"
-      @click.stop="onClick(item)"
-      @mouseover="onMouseOver(item)"
-      @mouseleave="onMouseLeave(item)"
-      :style="{
-        color: item.color,
-      }"
-    >
-      <div v-if="item.groupTitle" class="bimdata-menu__item--title">
-        <slot name="groupTitle">
-          <span>{{ item.groupTitle }}</span>
-        </slot>
-      </div>
-      <li
-        v-if="item.text"
+    <template v-if="menuItems && menuItems.length > 0">
+      <div
+        :ref="`item-${item.key}`"
+        v-for="item in menuItems"
+        :key="item.key"
+        class="bimdata-menu__item flex items-center"
+        :class="[
+          item.divider ? 'bimdata-menu__item--divider' : '',
+          { hover: isItemHover && currentItemKey === item.key },
+          hasNoChildren(item) ? 'bimdata-menu__item--no-children' : '',
+        ]"
+        @click.stop="onClick(item)"
+        @mouseover="onMouseOver(item)"
+        @mouseleave="onMouseLeave(item)"
         :style="{
-          'background-color':
-            isItemHover && currentItemKey === item.key ? item.background : '',
-          display: 'flex',
+          color: item.color,
         }"
       >
-        <slot name="item" :item="item">
-          <BIMDataTextbox
-            :text="item.text"
-            :tooltip="false"
-            width="80%"
-            :style="{
-              marginLeft: childrenLeft ? (item.children ? '11px' : '24px') : '',
-            }"
-          />
-        </slot>
-        <template v-if="item.children">
-          <slot name="children">
-            <BIMDataIcon
-              name="chevron"
-              size="xxs"
-              :rotate="childrenLeft ? 180 : 0"
-              :margin="childrenLeft ? '' : '0 0 0 auto'"
+        <div v-if="item.groupTitle" class="bimdata-menu__item--title">
+          <slot name="groupTitle">
+            <span>{{ item.groupTitle }}</span>
+          </slot>
+        </div>
+        <li
+          v-if="item.text"
+          :style="{
+            'background-color':
+              isItemHover && currentItemKey === item.key ? item.background : '',
+            display: 'flex',
+          }"
+        >
+          <slot name="item" :item="item">
+            <BIMDataTextbox
+              :text="item.text"
+              :tooltip="false"
+              width="80%"
               :style="{
-                order: childrenLeft ? -1 : 0,
+                marginLeft: childrenLeft
+                  ? item.children
+                    ? '11px'
+                    : '24px'
+                  : '',
               }"
             />
-            <template v-if="isItemHover">
-              <template
-                v-if="hasNoChildren(item) && currentItemKey === item.key"
-              >
-                <slot name="children-without-child" :children="item.children" />
-              </template>
-              <template v-else>
-                <ul
-                  :ref="`children-${item.key}`"
-                  class="bimdata-menu__item__children"
-                  :style="getChildrenStyle(item)"
-                >
-                  <slot name="child-header" :children="item.children" />
-                  <li
-                    v-for="child in item.children.list"
-                    :key="child.text"
-                    @click.stop="child.action && child.action()"
-                  >
-                    <slot name="child-item" :child="child">
-                      <BIMDataTextbox :text="child.text" :tooltip="false" />
-                    </slot>
-                  </li>
-                  <slot name="child-footer" :children="item.children" />
-                </ul>
-              </template>
-            </template>
           </slot>
+          <template v-if="item.children">
+            <slot name="children">
+              <BIMDataIcon
+                name="chevron"
+                size="xxs"
+                :rotate="childrenLeft ? 180 : 0"
+                :margin="childrenLeft ? '' : '0 0 0 auto'"
+                :style="{
+                  order: childrenLeft ? -1 : 0,
+                }"
+              />
+              <template v-if="isItemHover">
+                <template
+                  v-if="hasNoChildren(item) && currentItemKey === item.key"
+                >
+                  <slot
+                    name="children-without-child"
+                    :children="item.children"
+                  />
+                </template>
+                <template v-else>
+                  <ul
+                    :ref="`children-${item.key}`"
+                    class="bimdata-menu__item__children"
+                    :style="getChildrenStyle(item)"
+                  >
+                    <slot name="child-header" :children="item.children" />
+                    <li
+                      v-for="child in item.children.list"
+                      :key="child.text"
+                      @click.stop="child.action && child.action()"
+                    >
+                      <slot name="child-item" :child="child">
+                        <BIMDataTextbox :text="child.text" :tooltip="false" />
+                      </slot>
+                    </li>
+                    <slot name="child-footer" :children="item.children" />
+                  </ul>
+                </template>
+              </template>
+            </slot>
+          </template>
+        </li>
+        <template v-if="item.divider">
+          <div class="divider"></div>
         </template>
-      </li>
-      <template v-if="item.divider">
-        <div class="divider"></div>
-      </template>
-    </div>
+      </div>
+    </template>
+    <template v-else>
+      <slot class="bimdata-menu__elements" name="element"></slot>
+    </template>
   </ul>
 </template>
 
