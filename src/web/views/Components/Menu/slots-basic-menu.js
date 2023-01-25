f43f430a1839c66b48acf8f51e5c66794787d0d4
Gaelle <gaelle@bimdata.io>
2023-01-25 11:21:23 +0100

    add slot documentation

--- src/web/views/Components/Menu/slots-basic-menu.js
+++ src/web/views/Components/Menu/slots-basic-menu.js
@@ -1,7 +1,8 @@
 export default [
   ["Slot name", "Description"],
   ["groupTitle", "Use this slot for custom group title."],
-  ["item", "Use this slot for custom items menu."],
+  ["item", "Use this slot for custom items menu in v-for list items."],
+  ["element", "Use this slot to fully customize the personalized menu."],
   ["children", "Use this slot for custom children of menu items."],
   ["child-header", "Use this slot for custom child header."],
   ["child-item", "Use this slot for custom child items."],
