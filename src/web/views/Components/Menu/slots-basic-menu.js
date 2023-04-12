14bdb6070f5de2d36b0ee37da5ec99068c470851
Gaelle <gaelle@bimdata.io>
2023-04-12 10:03:44 +0200

    update slot documentation

--- src/web/views/Components/Menu/slots-basic-menu.js
+++ src/web/views/Components/Menu/slots-basic-menu.js
@@ -2,8 +2,9 @@ export default [
   ["Slot name", "Description"],
   ["groupTitle", "Use this slot for custom group title."],
   ["item", "Use this slot for custom items menu."],
-  ["children", "Use this slot for custom children of menu items."],
-  ["child-header", "Use this slot for custom child header."],
-  ["child-item", "Use this slot for custom child items."],
-  ["child-footer", "Use this slot for custom child footer."],
+  ["children-content", "Use this slot for custom children of menu items."],
+  // ["children", "Use this slot for custom children of menu items."],
+  // ["child-header", "Use this slot for custom child header."],
+  ["child-item", "Use this slot for custom list elements of child items."],
+  // ["child-footer", "Use this slot for custom child footer."],
 ];
