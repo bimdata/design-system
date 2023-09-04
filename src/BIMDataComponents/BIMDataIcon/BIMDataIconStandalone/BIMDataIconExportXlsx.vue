845cb0f09141405a144d4a65c4f9a04127c0e397
Pollo-droid <138143194+Pollo-droid@users.noreply.github.com>
2023-09-04 17:29:32 +0100

    PATCH: add export xls icon (#309)

---
+++ src/BIMDataComponents/BIMDataIcon/BIMDataIconStandalone/BIMDataIconExportXlsx.vue
@@ -0,0 +1,19 @@
+<template>
+  <Icon
+    :fill="fill"
+    :fillColor="fillColor"
+    :color="color"
+    :class="classes"
+    :style="style"
+  >
+    <BcfExportXlsx/>
+  </Icon>
+</template>
+  
+<script>
+import ExportXlsx from "../BIMDataLibraryIcons/ExportXlsx.vue";
+import makeIcon from "./iconFactory.js";
+export default makeIcon("ExportXlsx", ExportXlsx);
+</script>
+
+<style scoped lang="scss" src="../_BIMDataIcon.scss"></style>
