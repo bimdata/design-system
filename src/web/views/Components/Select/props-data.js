163b960fdb92f8503b45f57bec40d6086ba777ee
Gaelle <gaelle@bimdata.io>
2023-10-05 17:39:49 +0200

    add reset search documentation

--- src/web/views/Components/Select/props-data.js
+++ src/web/views/Components/Select/props-data.js
@@ -1,40 +1,14 @@
 /* eslint-disable */
 export default [
+  ["Name", "Type", "Default value", "Description"],
+  ["disabled", "Boolean", "false", "When true the selector will be disabled."],
   [
-    "Name", "Type", "Default value", "Description"
+    "isResetSearch",
+    "Boolean",
+    "false",
+    "In multi-selection mode: clear search bar on click away. In single selection: clear search bar on select element or click away.",
   ],
-  [
-    "width",
-    "[String, Number]",
-    "100%",
-    "Set the selector width.",
-  ],
-  [
-    "label",
-    "String",
-    "null",
-    "Set the selector label (placeholder)."
-  ],
-  [
-    "options",
-    "Array",
-    "[]",
-    "Array of available options. Can be an array of strings or objects (not both at the same time).",
-  ],
-  [
-    "optionKey",
-    "String",
-    "null",
-    "Only use this props if the 'options' props is an array of objects. "
-    + "Specify which field of the object represent the option value.",
-  ],
-  [
-    "optionLabelKey",
-    "String",
-    "null",
-    "Only use this props if the 'options' props is an array of objects. "
-    + "Specify which field of the object represent the option label.",
-  ],
+  ["label", "String", "null", "Set the selector label (placeholder)."],
   [
     "modelValue",
     "[String, Object, Array]",
@@ -48,34 +22,45 @@ export default [
     "Use this boolean to select multiple options from a list of options.",
   ],
   [
-    "search",
-    "Boolean",
-    "false",
-    "Use this boolean to add a search bar.",
-  ],
-  [
-    "searchPlaceholder",
+    "nullLabel",
     "String",
-    "Search",
-    "Use this props to custom search placeholder.",
+    "",
+    "Define a custom label for `null` or `undefined` options.",
   ],
   [
     "nullValue",
     "Boolean",
     "false",
-    "This props has no effect in multi-selection mode. "
-    + "Add a 'none' value in single-selection mode.",
+    "This props has no effect in multi-selection mode. " +
+      "Add a 'none' value in single-selection mode.",
   ],
   [
-    "nullLabel",
+    "optionKey",
     "String",
-    "",
-    "Define a custom label for `null` or `undefined` options."
+    "null",
+    "Only use this props if the 'options' props is an array of objects. " +
+      "Specify which field of the object represent the option value.",
   ],
   [
-    "disabled",
-    "Boolean",
-    "false",
-    "When true the selector will be disabled."
-  ]
+    "optionLabelKey",
+    "String",
+    "null",
+    "Only use this props if the 'options' props is an array of objects. " +
+      "Specify which field of the object represent the option label.",
+  ],
+  [
+    "options",
+    "Array",
+    "[]",
+    "Array of available options. Can be an array of strings or objects (not both at the same time).",
+  ],
+  ["search", "Boolean", "false", "Use this boolean to add a search bar."],
+  [
+    "searchPlaceholder",
+    "String",
+    "Search",
+    "Use this props to custom search placeholder.",
+  ],  
+  ["width", "[String, Number]", "100%", "Set the selector width."],
+
 ];
