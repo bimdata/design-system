9f74454c9ad61f4e333dd9b80a8180e7bd1e1852
Gaelle <gaelle@bimdata.io>
2024-03-07 14:32:40 +0100

    add Caret icon

---
+++ src/BIMDataComponents/BIMDataIcon/BIMDataIconStandalone/BIMDataIconCaret.vue
@@ -0,0 +1,19 @@
+<template>
+  <Icon
+    :fill="fill"
+    :fillColor="fillColor"
+    :color="color"
+    :class="classes"
+    :style="style"
+  >
+    <Caret />
+  </Icon>
+</template>
+
+<script>
+import Caret from "../BIMDataLibraryIcons/Caret.vue";
+import makeIcon from "./iconFactory.js";
+export default makeIcon("Caret", Caret);
+</script>
+
+<style scoped lang="scss" src="../_BIMDataIcon.scss"></style>
--- src/BIMDataComponents/BIMDataIcon/BIMDataLibraryIcons/index.js
+++ src/BIMDataComponents/BIMDataIcon/BIMDataLibraryIcons/index.js
@@ -20,6 +20,8 @@ import calendar from "./Calendar.vue";
 import caliper from "./Caliper.vue";
 import camera from "./Camera.vue";
 import cancelCamera from "./CancelCamera.vue";
+import caret from "./Caret.vue";
+
 import check from "./Check.vue";
 import chevron from "./Chevron.vue";
 import classificationCategory from "./ClassificationCategory.vue";
@@ -189,6 +191,7 @@ export default {
   caliper,
   camera,
   cancelCamera,
+  caret,
   check,
   chevron,
   classificationCategory,
