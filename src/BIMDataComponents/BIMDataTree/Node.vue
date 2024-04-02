d0581c69231c471f347ef103918a14fbf7491d97
GAETAN LAGIER <gaetan@bimdata.io>
2024-04-02 17:00:09 +0200

    remove circular dependency in BIMDataTree

--- src/BIMDataComponents/BIMDataTree/Node.vue
+++ src/BIMDataComponents/BIMDataTree/Node.vue
@@ -48,9 +48,39 @@
 </template>
 
 <script>
-import { inject, ref, computed, watch, isRef } from "vue";
+import { inject, ref, computed, watch, isRef, h } from "vue";
 
-import NodeChildren from "./NodeChildren.js";
+const NodeChildren = {
+  inject: ["root"],
+  props: {
+    node: {
+      type: Object,
+      required: true,
+      validator: node => Number.isFinite(node?.id),
+    },
+    depth: {
+      type: Number,
+      default: 0,
+    },
+  },
+  render() {
+    const { node, depth, root } = this;
+
+    const state = root.state;
+
+    return node.children.map(child =>
+      h(Node, { node: child, depth, key: child.id }, () =>
+        root.$slots.node?.({
+          node: child,
+          depth,
+          selected: child.id === state.selectedId,
+          hovered: child.id === state.hoveredNode?.id,
+          ancestorSelected: state.hasAncestorSelected(child),
+        })
+      )
+    );
+  },
+};
 
 export default {
   name: "Node",
