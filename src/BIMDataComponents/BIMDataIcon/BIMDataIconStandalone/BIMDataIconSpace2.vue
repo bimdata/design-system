72e6eb0c2e373d17078ec7cfcecf7da86a61b675
Nicolas Richel <nicolas@bimdata.io>
2024-04-04 10:12:11 +0200

    PATCH: feat(icons): add space2 and zone2 icons (#346)

---
+++ src/BIMDataComponents/BIMDataIcon/BIMDataIconStandalone/BIMDataIconSpace2.vue
@@ -0,0 +1,19 @@
+<template>
+  <Icon
+    :fill="fill"
+    :fillColor="fillColor"
+    :color="color"
+    :class="classes"
+    :style="style"
+  >
+    <Space2 />
+  </Icon>
+</template>
+
+<script>
+import Space2 from "../BIMDataLibraryIcons/Space2.vue";
+import makeIcon from "./iconFactory.js";
+export default makeIcon("Space2", Space2);
+</script>
+
+<style scoped lang="scss" src="../_BIMDataIcon.scss"></style>
---
+++ src/BIMDataComponents/BIMDataIcon/BIMDataIconStandalone/BIMDataIconZone2.vue
@@ -0,0 +1,19 @@
+<template>
+  <Icon
+    :fill="fill"
+    :fillColor="fillColor"
+    :color="color"
+    :class="classes"
+    :style="style"
+  >
+    <Zone2 />
+  </Icon>
+</template>
+
+<script>
+import Zone2 from "../BIMDataLibraryIcons/Zone2.vue";
+import makeIcon from "./iconFactory.js";
+export default makeIcon("Zone2", Zone2);
+</script>
+
+<style scoped lang="scss" src="../_BIMDataIcon.scss"></style>
