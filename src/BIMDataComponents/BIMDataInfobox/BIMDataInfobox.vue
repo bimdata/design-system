5311fa4e0564bb8d2aacdbbbf69d9489a88acf4e
Nicolas Richel <nicolas@bimdata.io>
2024-09-05 14:48:02 +0200

    MINOR: feat: add BIMDataInfobox component (#368)

---
+++ src/BIMDataComponents/BIMDataInfobox/BIMDataInfobox.vue
@@ -0,0 +1,71 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    default: ""
+  },
+  icon: {
+    type: String,
+    default: "information"
+  },
+  color: {
+    type: String,
+    validator:
+      v => [
+        "granite-light",
+        "high",
+        "neutral",
+        "primary",
+        "secondary",
+        "success",
+        "warning",
+      ].includes(v),
+    default: "neutral"
+  },
+});
+</script>
+
+<template>
+  <div class="bimdata-infobox">
+    <div class="bimdata-infobox__stripe"
+      :style="{ backgroundColor: `var(--color-${color})` }"
+    >
+      <!-- left stripe -->
+    </div>
+    <div class="bimdata-infobox__icon">
+      <BIMDataIcon :name="icon" :customSize="18" :color="color" fill />
+    </div>
+    <div class="bimdata-infobox__text">
+      <slot>{{ text }}</slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bimdata-infobox {
+  position: relative;
+  padding: var(--spacing-unit);
+  border-radius: 3px;
+  background-color: var(--color-silver-light);
+  color: var(--color-granite-light);
+  overflow: hidden;
+
+  display: flex;
+  align-items: start;
+  gap: var(--spacing-unit);
+
+  .bimdata-infobox__stripe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 4px;
+    height: 100%;
+  }
+
+  .bimdata-infobox__icon {
+    width: 18px;
+    height: 18px;
+    margin-top: 2px;
+  }
+}
+</style>
