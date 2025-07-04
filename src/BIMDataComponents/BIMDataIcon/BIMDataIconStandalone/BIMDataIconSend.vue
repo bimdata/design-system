5197fe4e9e4d396c56d38aec1c566a46b3792f76
Gaelle <gaelle@bimdata.io>
2025-07-04 10:29:46 +0200

    add mic, send & trash icons

---
+++ src/BIMDataComponents/BIMDataIcon/BIMDataIconStandalone/BIMDataIconSend.vue
@@ -0,0 +1,19 @@
+<template>
+  <Icon
+    :fill="fill"
+    :fillColor="fillColor"
+    :color="color"
+    :class="classes"
+    :style="style"
+  >
+    <Send />
+  </Icon>
+</template>
+
+<script>
+import Send from "../BIMDataLibraryIcons/Send.vue";
+import makeIcon from "./iconFactory.js";
+export default makeIcon("Send", Send);
+</script>
+
+<style scoped src="../BIMDataIcon.css"></style>
---
+++ src/BIMDataComponents/BIMDataIcon/BIMDataIconStandalone/BIMDataIconTrash.vue
@@ -0,0 +1,19 @@
+<template>
+  <Icon
+    :fill="fill"
+    :fillColor="fillColor"
+    :color="color"
+    :class="classes"
+    :style="style"
+  >
+    <Trash />
+  </Icon>
+</template>
+
+<script>
+import Trash from "../BIMDataLibraryIcons/Trash.vue";
+import makeIcon from "./iconFactory.js";
+export default makeIcon("Trash", Trash);
+</script>
+
+<style scoped src="../BIMDataIcon.css"></style>
