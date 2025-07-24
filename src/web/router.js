16d4c913c32933cf12c2521bd979a00327faf4bb
Gaelle <gaelle@bimdata.io>
2025-07-24 15:42:58 +0200

    fix router

--- src/web/router.js
+++ src/web/router.js
@@ -238,7 +238,7 @@ const routes = [
         component: () => import("./views/Components/Textbox/Textbox.vue"),
       },
       {
-        path: "Textbox",
+        path: "toggle",
         name: "toggle",
         component: () => import("./views/Components/Toggle/Toggle.vue"),
       },
