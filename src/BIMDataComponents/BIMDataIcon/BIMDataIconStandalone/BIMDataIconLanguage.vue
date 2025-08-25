f93d31a40a472cca1423c778c8a9b2f78df20a31
Gaelle <gaelle@bimdata.io>
2025-08-25 15:46:23 +0200

    add 5 icons : - language - logout - marketplace - offline - quit

---
+++ src/BIMDataComponents/BIMDataIcon/BIMDataIconStandalone/BIMDataIconLanguage.vue
@@ -0,0 +1,20 @@
+<template>
+  <Icon
+    :fill="fill"
+    :fillColor="fillColor"
+    :color="color"
+    :class="classes"
+    :style="style"
+  >
+    <Language />
+  </Icon>
+</template>
+
+<script>
+import Language from "../BIMDataLibraryIcons/Language.vue";
+import makeIcon from "./iconFactory.js";
+
+export default makeIcon("Language", Language);
+</script>
+
+<style scoped src="../BIMDataIcon.css"></style>
---
+++ src/BIMDataComponents/BIMDataIcon/BIMDataIconStandalone/BIMDataIconMarketplace.vue
@@ -0,0 +1,20 @@
+<template>
+  <Icon
+    :fill="fill"
+    :fillColor="fillColor"
+    :color="color"
+    :class="classes"
+    :style="style"
+  >
+    <Marketplace />
+  </Icon>
+</template>
+
+<script>
+import Marketplace from "../BIMDataLibraryIcons/Marketplace.vue";
+import makeIcon from "./iconFactory.js";
+
+export default makeIcon("Marketplace", Marketplace);
+</script>
+
+<style scoped src="../BIMDataIcon.css"></style>
