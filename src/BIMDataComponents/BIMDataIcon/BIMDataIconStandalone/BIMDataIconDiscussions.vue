dc9ff29f70a7298b9407a16dbaba628eb6c6ceb1
NicolasRichel <nicolas@bimdata.io>
2025-10-17 15:28:13 +0200

    PATCH: feat(icon): add 'discussions' and 'issues' icons

---
+++ src/BIMDataComponents/BIMDataIcon/BIMDataIconStandalone/BIMDataIconDiscussions.vue
@@ -0,0 +1,20 @@
+<template>
+  <Icon
+    :fill="fill"
+    :fillColor="fillColor"
+    :color="color"
+    :class="classes"
+    :style="style"
+  >
+    <Detach />
+  </Icon>
+</template>
+
+<script>
+import Discussions from "../BIMDataLibraryIcons/Discussions.vue";
+import makeIcon from "./iconFactory.js";
+
+export default makeIcon("Discussions", Discussions);
+</script>
+
+<style scoped src="../BIMDataIcon.css"></style>
---
+++ src/BIMDataComponents/BIMDataIcon/BIMDataIconStandalone/BIMDataIconIssues.vue
@@ -0,0 +1,20 @@
+<template>
+  <Icon
+    :fill="fill"
+    :fillColor="fillColor"
+    :color="color"
+    :class="classes"
+    :style="style"
+  >
+    <Detach />
+  </Icon>
+</template>
+
+<script>
+import Issues from "../BIMDataLibraryIcons/Issues.vue";
+import makeIcon from "./iconFactory.js";
+
+export default makeIcon("Issues", Issues);
+</script>
+
+<style scoped src="../BIMDataIcon.css"></style>
